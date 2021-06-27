2e93358ecb535c04c2ddfc3e539b0ba934dc3d06
Woodstock <victorhe97@gmail.com>
2021-06-26 23:21:29 -0400

    First Python program

---
+++ HelloWorld.py
@@ -0,0 +1,12 @@
+print('Hello, world!')
+
+print('What is your name?')
+myName = input()
+print('It\'s nice to meet you,' + myName)
+
+print('The length of your name is: ')
+print(len(myName))
+
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + 'in a year.')
